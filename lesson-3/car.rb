76aadbc280e5e0ba4b381b8b9eb0f59ca9eee384
Lokideos <Lokideos>
2017-03-15 00:33:31 +0300

    Add files from class work

--- lesson-3/car.rb
+++ lesson-3/car.rb
@@ -1,3 +1,26 @@
-class Car
-  def 
+class Car  
+  attr_accessor :speed
+
+  attr_reader :engine_volume  
+
+  def initialize(speed = 0, engine_volume ) 
+    @speed = speed
+    @engine_volume = engine_volume    
+  end
+
+  def start_engine
+    puts "Wroom!"
+  end
+
+  def beep
+    puts "beep beep"
+  end
+
+  def stop
+    self.speed = 0
+  end  
+
+  def go
+    self.speed = 50
+  end
 end
